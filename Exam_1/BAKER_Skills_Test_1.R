47eb375751f2755b8d6516c4e1d1d19db4031b54
Shannon Baker <shannonbaker435@gmail.com>
2021-02-12 10:54:36 -0700

    final version

--- Exam_1/BAKER_Skills_Test_1.R
+++ Exam_1/BAKER_Skills_Test_1.R
@@ -88,31 +88,17 @@ Katy_means <- aggregate(x= csv$DNA_Concentration_Katy,
 means_combined <- cbind(Ben_means$Group.1, Ben_means$x,Katy_means$x)
 colnames(means_combined) <- c("Year","Ben_mean","Katy_mean")
 
-ggplot(x=means_combined$Ben_means, y= means_combined$Katy_means)
 
-plot(x=(means_combined$Ben_means & means_combined$Katy_means), y=means_combined$Year))
-plot(y=Ben_means$x , x= Ben_means$Group.1)
-plot(y=Katy_means$x , x= Katy_means$Group.1)
-
-means$Ben_mean <- Ben_means
-means$Katy_mean <- Katy_means
-names(means)
-ggplot(means, aes(y=Ben_mean & Katy_mean, x=TRUE))
-ggplot(Katy_means $ Ben_means, aes(y=Ben_means$x & Katy_means$x, x= Ben_means$Group.1 &Katy_means$Group.1 ))
-
-ggplot(NULL, aes(x=Ben_means$x,  y=  Katy_means$x, col=year))
-
-ggplot (means_combined, aes(x= Ben_mean, y= Katy_mean, col=Year))+
-          geom_point()
-
-means_combined$Ben_mean / means_combined$Katy_mean
-
-means_combined$difference <- means_combined$Ben_mean - means_combined$Katy_mean
+class(means_combined)
+means_combined <- as.data.frame(means_combined)
 
 
-class(means_combined$Ben_mean)
+#min is when Ben did the worst relative to Katy; found by dividing Ben's values by Katy's values
+min(means_combined$Ben_mean / means_combined$Katy_mean)
+means_combined[which((means_combined$Ben_mean / means_combined$Katy_mean) == min(means_combined$Ben_mean / means_combined$Katy_mean)), ]
 
-rlang::last_error()
+print("it seems that in year 2010, Ben did his worst compared to Katy, but he did his max DNA extraction in 2007")
+
 
 #V.
 
