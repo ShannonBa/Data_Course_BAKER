84e01f2aeb9c8a94be84845c9d984197928adfd7
Shannon Baker <shannonbaker435@gmail.com>
2021-02-12 08:19:24 -0700

    more recent

--- Exam_1/BAKER_Skills_Test_1.R
+++ Exam_1/BAKER_Skills_Test_1.R
@@ -67,17 +67,52 @@ plot(x=year, y=csv$DNA_Concentration_Ben,
 dev.off()
 
 #IV.
+#Your task here is to write some code that tells us: in which 
+#extraction YEAR, was Ben's performance the
+#lowest RELATIVE TO Katy's performance?
+
 Katy <- mean(csv$DNA_Concentration_Katy)
 Ben <- mean(csv$DNA_Concentration_Ben)
 Ben>Katy
 K_B <- c(Katy,Ben)
 plot(x=K_B)
 
-#Your task here is to write some code that tells us: in which 
-#extraction YEAR, was Ben's performance the
-#lowest RELATIVE TO Katy's performance?
+Ben_means <- aggregate(x= csv$DNA_Concentration_Ben,
+          by= list(csv$Year_Collected),
+          FUN=mean)
+
+Katy_means <- aggregate(x= csv$DNA_Concentration_Katy,
+          by= list(csv$Year_Collected),
+          FUN=mean)
+
+means_combined <- cbind(Ben_means$Group.1, Ben_means$x,Katy_means$x)
+colnames(means_combined) <- c("Year","Ben_mean","Katy_mean")
+
+ggplot(x=means_combined$Ben_means, y= means_combined$Katy_means)
+
+plot(x=(means_combined$Ben_means & means_combined$Katy_means), y=means_combined$Year))
+plot(y=Ben_means$x , x= Ben_means$Group.1)
+plot(y=Katy_means$x , x= Katy_means$Group.1)
+
+means$Ben_mean <- Ben_means
+means$Katy_mean <- Katy_means
+names(means)
+ggplot(means, aes(y=Ben_mean & Katy_mean, x=TRUE))
+ggplot(Katy_means $ Ben_means, aes(y=Ben_means$x & Katy_means$x, x= Ben_means$Group.1 &Katy_means$Group.1 ))
+
+ggplot(NULL, aes(x=Ben_means$x,  y=  Katy_means$x, col=year))
+
+ggplot (means_combined, aes(x= Ben_mean, y= Katy_mean, col=Year))+
+          geom_point()
+
+means_combined$Ben_mean / means_combined$Katy_mean
+
+means_combined$difference <- means_combined$Ben_mean - means_combined$Katy_mean
 
 
+class(means_combined$Ben_mean)
+
+rlang::last_error()
 
 #V.
 
@@ -99,8 +134,13 @@ dev.off()
 
 #VI.
 
-Just_Ben<- csv[Date_Collected,]
+Ben_Average_Conc <- aggregate(x= csv$DNA_Concentration_Ben,
+          by= list(csv$Year_Collected),
+          FUN=mean)
+colnames(Ben_Average_Conc) <- c("Year","Ben_means")
 
+Ben_Average_Conc[which((Ben_Average_Conc$Ben_means) == max(Ben_Average_Conc$Ben_means)), ]
 
+write.csv(Ben_Average_Conc, "Ben_Average_Conc.csv")
 
 
