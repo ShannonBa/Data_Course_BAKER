eba5f461d90b866f938ba0fdb3c02c74dec39fe4
Shannon Baker <shannonbaker435@gmail.com>
2021-04-15 09:27:56 -0600

    Almost complete exam 3

---
+++ Exam_3/Exam_3/BAKER_Skills_Test_3.R
@@ -0,0 +1,101 @@
+################ BAKER_Skills_Test_3 ######################
+
+
+#### Load libraries ####
+
+library(tidyverse)
+library(janitor)
+library(broom)
+library(modelr)
+
+
+#### Task I. #### 
+
+df <- read.csv("./FacultySalaries_1995.csv")
+names(df) <- make_clean_names(names(df))
+names(df)
+
+df <- df %>%
+  pivot_longer(c(avg_full_prof_salary,avg_assoc_prof_salary, avg_assist_prof_salary),
+                names_prefix = "_salary", 
+               values_to = "salary",
+               names_to = "rank")
+df <- df[!df$tier %in% "VIIB", ]
+
+
+ggplot(df, aes(x= rank, y = salary, fill =rank)) +
+  geom_boxplot( size = 1, outlier.size=3) +
+  facet_wrap(~tier)+
+  theme_minimal()+
+  theme(axis.text.x = element_text(angle = 45)) +
+  labs(y="Salary",x="Rank", fill = "Rank" )+
+  scale_fill_discrete(name = "Rank", labels = c("Assist", "Assoc", "Full"))+
+  scale_x_discrete(labels=c("avg_full_prof_salary" = "Full", 
+                            "avg_assoc_prof_salary" = "Assoc",
+                            "avg_assist_prof_salary" = "Assist"))
+ggsave("BAKER_Fig_1.jpg")
+
+
+#ggsave("BAKER_Fig_1.jpg")
+#everything looks good except my graphs are wider....
+
+
+#### Task II. #### 
+# influence of "State", "Tier", and "Rank" on "Salary" 
+
+aov.df <- aov(salary ~ tier + rank + state,  data = df)
+summary(aov.df)
+summary(aov.df)
+capture.output(summary(aov.df), 'output.txt') 
+#a <- #
+
+cat(text_lines[aov.df], file = "Salary_ANOVA_Summary.txt", sep = "\n") 
+dput(summary(aov(salary ~ tier + rank + state,  data = df),file="Salary_ANOVA_Summary.txt",control="all"))
+
+write.table(summary(aov.df), file = "Salary_ANOVA_Summary.txt", sep = "\t",
+            row.names = TRUE, col.names = NA)
+
+
+
+
+
+
+
+
+#### Task III. ####
+
+df2 <- read.csv("./Juniper_Oils.csv")
+names(df2)
+df2 <- df2 %>%
+  pivot_longer(c("alpha.pinene","para.cymene","alpha.terpineol","cedr.9.ene","alpha.cedrene",
+                 "beta.cedrene","cis.thujopsene","alpha.himachalene","beta.chamigrene",
+                 "cuparene","compound.1","alpha.chamigrene","widdrol","cedrol","beta.acorenol",
+                 "alpha.acorenol","gamma.eudesmol","beta.eudesmol","alpha.eudesmol",
+                 "cedr.8.en.13.ol","cedr.8.en.15.ol","compound.2","thujopsenal"),
+             values_to = "Concentration",
+             names_to = "ChemicalID")
+
+
+
+#### Task IV. ####
+
+
+ggplot(df2, aes(x= YearsSinceBurn, y = Concentration)) +
+  geom_smooth() +
+  facet_wrap(~ChemicalID, scales = "free_y") +
+  theme_minimal()
+ggsave("BAKER_Fig_2.jpg")
+
+
+#### Task V. ####
+
+mod1 <- glm(data = df2, Concentration ~ YearsSinceBurn * ChemicalID)
+
+step <- stepAIC(mod1)
+mod1#widdles down your variables to the best model possible
+
+step$call #gives the model that was decidedly the best
+names(df2)
+
+summary(mod1)
+
